cfb29a956530b188337e9c92a2ae66c643d8d581
gjbaker <gregory.j.baker@gmail.com>
2026-03-03 14:07:31 -0700

    Remove bad, unnecessary file copy & rename test

--- tests/test_pdf_read.py
+++ tests/test_pdf_read.py
@@ -3,24 +3,26 @@
 
 import os
 from pathlib import Path
-from shutil import copyfile
 import pymupdf
+
+import app_modules.file_locations as loctns
+import app_modules.utilities as utils
 
 
 # Prepare for testing
-DATA = 'tests/data/'
-FNAME = 'hlap Aug 2023 cycle 2'
-copyfile(f'{DATA}archive/{FNAME}.pdf', FNAME)
+TEST_DATA = Path(loctns.TEST_DATA)
+utils.initialize_log_file(path=utils.FILE_PATH)
+FNAME = 'hlap Aug 2023 cycle 2.pdf'
 
 
 def test_page_count():
     """Checks number of pages in PDF document."""
     #delete files that may interfere with testing
-    for file in Path(DATA).iterdir():
+    for file in Path(TEST_DATA).iterdir():
         if file.match('B47001*.*'):
             os.remove(file)
 
-    with pymupdf.open(FNAME) as pdf:
+    with pymupdf.open(TEST_DATA / 'archive' / FNAME) as pdf:
         num = 0
         for num, page in enumerate(pdf):
             text = page.get_text()
